31b04f854c9adcb5dafb1742bf5fdaae9aeedae5
anysome <anysome@gmail.com>
2016-05-13 20:37:54 +0800

    upgrade modules

--- app/views/inlet/Splash.js
+++ app/views/inlet/Splash.js
@@ -3,7 +3,7 @@
  */
 import React from 'react';
 import {View, Text} from 'react-native';
-import Animatable from 'react-native-animatable';
+import * as Animatable from 'react-native-animatable';
 
 export default class Splash extends React.Component {
   render() {
