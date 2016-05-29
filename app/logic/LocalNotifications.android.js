a195d7539f41d09b9735ef601803ad2ce876ea81
anysome <anysome@gmail.com>
2016-05-29 11:38:36 +0800

    android local notification

--- app/logic/LocalNotifications.android.js
+++ app/logic/LocalNotifications.android.js
@@ -7,7 +7,7 @@ import Notification from 'react-native-system-notification';
 function hash(str) {
   let hash = 0, i, chr, len;
   if (str.length === 0) return hash;
-  for (i = 0, len = this.length; i < len; i++) {
+  for (i = 0, len = str.length; i < len; i++) {
     chr   = str.charCodeAt(i);
     hash  = ((hash << 5) - hash) + chr;
     hash |= 0; // Convert to 32bit integer
@@ -15,11 +15,34 @@ function hash(str) {
   return hash;
 }
 
+function noop() {
+  // do nothing for promise chain
+}
+
+Notification.addListener('press', function(e) {
+  // console.log(JSON.stringify(e));
+  // stop repeating when user click notification
+  let payload = e.payload;
+  if ( payload ) {
+    let nid = hash(payload.type + ": " + payload.id);
+    // cancel
+    Notification.find(nid).then(function(notification) {
+      Notification.delete(nid).then(noop, noop);
+    }, noop);
+  } else {
+    Notification.clearAll().then(noop, noop);
+  }
+});
+
 export default class LocalNotifications {
 
   static scheduleAgenda(agenda) {
+    let nid = hash('agenda: ' + agenda.id);
+    console.log('notification id = ' + nid);
     // cancel old
-    Notification.delete(hash('agenda: ' + agenda.id));
+    Notification.find(nid).then(function(notification) {
+      Notification.delete(nid).then(noop, noop);
+    }, noop);
     // schedule new if necessary
     if (agenda.reminder) {
       let now = new Date();
@@ -33,16 +56,22 @@ export default class LocalNotifications {
       let fireDate = new Date(alarmTime);
       fireDate.setSeconds(1);
       Notification.create({
+        id: nid,
         subject: agenda.title,
         message: agenda.detail || '行事易提醒您该干活了哦~',
+        payload: {type:'agenda', id: agenda.id},
         sendAt: fireDate,
         repeatEvery: 'minute',
-        count: 1
+        repeatCount: 3
       });
     }
   }
 
   static cancelAgenda(id) {
-    Notification.delete(hash('agenda: ' + agenda.id));
+    let nid = hash('agenda: ' + id);
+    console.log('notification id = ' + nid);
+    Notification.find(nid).then(function(notification) {
+      Notification.delete(nid).then(noop, noop);
+    }, noop);
   }
 };
